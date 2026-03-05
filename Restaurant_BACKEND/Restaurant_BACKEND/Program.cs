a15d3eafe4d1fb905fb9f1a8f0b76fa45904f6dd
anamaria1203 <anamaria.vasile2005112@gmail.com>
2026-03-05 13:02:20 +0200

    buna

--- Restaurant_BACKEND/Restaurant_BACKEND/Program.cs
+++ Restaurant_BACKEND/Restaurant_BACKEND/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // test modificare
 //alabamadl
+//ana e tare smechera
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
